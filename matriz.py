39c6195d80fb1b1dd5d8983db484de4f2081c292
RaulToto <djbrexman@gmail.com>
2016-07-06 18:47:52 -0500

    ultima actualizacion

--- matriz.py
+++ matriz.py
@@ -13,9 +13,7 @@ def mostrarMatrices(row,cols,matriz):
 def inicializarMatriz(row,cols,matriz):
     for i in range(row):
         matriz.append(["-"]*cols)
-    print("-----------Matriz1 inicilaizada------------")
-    mostrarMatrices(row,cols,matriz)#imprime la matriz 1
-    #inicializa la matriz2 con 0
+    #incializa la matriz con "-"
 def llenarMatriz(row,cols,matriz):
     for i in range(0,row): #bucle para pedir al usuario que llene la matriz1
         for j in range(0,cols):
@@ -25,33 +23,40 @@ def llenarMatriz(row,cols,matriz):
     mostrarMatrices(row,cols,matriz)
         
 def matriz(x):
-    print("----------------Welcome--------------")
+   # print("----------------Welcome--------------")
     matriz1=[]
     matriz2=[]
     matriz3=[]
     
     if x==1 or x==2 or x==3:
+        print("-----------Matriz1----------")
         row=int(input("ingrese la fila"))
         cols=int(input("ingrese las columna"))
+        print("-----------matriz2----------")
         fila=int(input("ingrese las fila"))
         columna=int(input("ingrese las columna"))
-    
-
-        inicializarMatriz(row,cols,matriz1)
-        inicializarMatriz(fila,columna,matriz2)
-        inicializarMatriz(row,columna,matriz3)
-        print("llene los elementos")
-        print("---------Matriz1------------")
-        llenarMatriz(row,cols,matriz1)
-        print("---------matriz2------------")
-        llenarMatriz(fila,columna,matriz2)
-        if x==1:
-            sumaMatrices(matriz1,matriz2,matriz3,row,cols)
-        elif x==2:
-            restaMatrices(matriz1,matriz2,matriz3,row,cols)
-        elif x==3:
-            print("multiplicaion")
-            multiplicacionMatrices(matriz1,matriz2,matriz3,row,columna,fila)
+        if ((x==1 or x==2) and (row!=fila or cols!=columna)) or (x==3 and cols!=fila):
+            if x==1 or x==2:
+                print("recuerde que si quiere sumar o restar la matrices deben ser iguales")
+            else:
+                print("recuerde que la multiplicacion es de tipo (Aij)x(Bjk)")
+        else: 
+            inicializarMatriz(row,cols,matriz1)
+            inicializarMatriz(fila,columna,matriz2)
+            inicializarMatriz(row,columna,matriz3)
+            print("llene los elementos")
+            print("---------Matriz1------------")
+            llenarMatriz(row,cols,matriz1)
+            print("---------matriz2------------")
+            llenarMatriz(fila,columna,matriz2)
+            if x==1:
+                print("-------------------------------------")
+                sumaMatrices(matriz1,matriz2,matriz3,row,cols)
+            elif x==2:
+                restaMatrices(matriz1,matriz2,matriz3,row,cols)
+            elif x==3:
+                print("multiplicaion")
+                multiplicacionMatrices(matriz1,matriz2,matriz3,row,columna,fila)
     elif x==4 or x==5:
         row=int(input("ingrese la fila"))
         cols=int(input("ingrese las columna"))
@@ -59,16 +64,21 @@ def matriz(x):
         print("llene los elementos")
         print("---------Matriz1------------")
         llenarMatriz(row,cols,matriz1)
-        print("la matriz  de adiacencia es ")
+        
         if x==4:
-             warsall(matriz1,row,cols)
+            print("la matriz  de adiacencia es ")
+            warsall(matriz1,row,cols)
         elif x==5:
+            print("la transapuesta es ")
             transapuesta(matriz1,row,cols)
 def sumaMatrices(matriz1,matriz2,matriz3,row,cols):
     for i in range(0,row):
         for j in range(0,cols):
             matriz3[i][j]=matriz1[i][j] + matriz2[i][j]
-    print("-------------la suma es-------------------------------------")
+    mostrarMatrices(row,cols,matriz1)
+    print(" +           ")
+    mostrarMatrices(row,cols,matriz2)
+    print(" =           ")
     mostrarMatrices(row,cols,matriz3)
 def restaMatrices(matriz1,matriz2,matriz3,row,cols):
     for i in range(0,row):
@@ -76,38 +86,6 @@ def restaMatrices(matriz1,matriz2,matriz3,row,cols):
             matriz3[i][j]=matriz1[i][j] - matriz2[i][j]
     print("----------------------la  resta es-------------------------------------")
     mostrarMatrices(row,cols,matriz3)
-
-'''       
-def incializaMatriz(row,cols):   
-    print("matrices boolenas ")
-    matriz1=[]
-    matriz2=[]
-    matriz3=[]
-    #inicializa la matriz 1 con 0
-    for i in range(row):
-        matriz1.append([0]*cols)
-    print(matriz1)#imprime la matriz 1
-    #inicializa la matriz2 con 0
-    for i in range(row):
-        matriz2.append([0]*cols)
-    print(matriz2)#imprime la matriz 2
-    for i in range(row):
-        matriz3.append([0]*cols)
-    print(matriz3)#imprime la matriz 3
-def inicializar():
-            print("A[",i,",",j,"]")#imprime la posicion
-    for i in range(0,row): #bucle para pedir al usuario que llene la matriz1
-        for j in range(0,cols):
-            matriz1[i][j]=int(input("ingrese un elemento"))#el usuario ingresa un elemento de la matriz
-            print(matriz1)
-    print("--------------Matriz 2--------------------------------")
-    for i in range(0,row):#bucle para pedir al usuario que llene la matriz2
-        for j in range(0,cols):
-            print("B[",i,",",j,"]")
-            matriz2[i][j]=int(input("ingrese los elementos de la matriz"))
-            print(matriz2)
- 
-'''
 def multiplicacionMatrices(matriz1,matriz2,matriz3,row,columna,fila):
     for i in range(0,row):
         for j in range(0,columna):
@@ -129,4 +107,5 @@ def transapuesta(matriz1,row,cols):
             matriz1[i][j]=matriz1[j][i]
             matriz1[j][i]=temporal
         cont+=1
+    mostrarMatrices(row,cols,matriz1)
 inicio()
